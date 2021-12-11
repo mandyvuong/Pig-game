4a2bc3f91bbbe0e67e607bde106788f3ff2eea98
amanda vuong <mandyvuong@gmail.com>
2021-12-11 21:22:02 +0000

    score for each player set to zero

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+// Selecting elements are equivalent but getElementById is faster than querySelector
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
