8f875db0dc1949c63e79cbe4a30649797ec1e3c9
amanda vuong <mandyvuong@gmail.com>
2021-12-11 21:40:40 +0000

    rolling dice for player0

--- script.js
+++ script.js
@@ -4,8 +4,36 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+
 const diceEl = document.querySelector('.dice');
 
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // Generate random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  // Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // Check roll 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // Switch to next player
+  }
+});
