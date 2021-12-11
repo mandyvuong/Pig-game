3bd22e99b10653a3e1ba56464ba553fbe60e5ce7
amanda vuong <mandyvuong@gmail.com>
2021-12-11 21:57:33 +0000

    switch player - css changes

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const current0El = document.querySelector('#current--0');
 const current1El = document.querySelector('#current--1');
 
@@ -41,5 +44,8 @@ btnRoll.addEventListener('click', function () {
     document.querySelector(`#current--${activePlayer}`).textContent = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
     currentScore = 0;
+    // check if class exist and change
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
